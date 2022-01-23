3088bf8614489e4855b3eeb125b901656fbd76c5
mizue-water <buzz.bugsy13.2id8.m.ysa@gmail.com>
2022-01-23 14:28:52 +0900

    game sample 03 add attack

--- js/rpg.js
+++ js/rpg.js
@@ -1,9 +1,62 @@
 window.onload = function() {
     // 画面表示関数を呼び出し
-    dispFields();
+    dispFields("");
 };
 
-function dispFields() {
+class Character {
+    constructor(name, hp) {
+      this.name = name;
+      this.hp = hp;
+    }
+    
+    attack() {
+      const attackPoint = Math.floor(Math.random() * 10);
+      return attackPoint;
+    }  
+}
+
+//主人公のクラスを作成する
+class Hero extends Character {
+    constructor(name, hp) {
+        super(name, hp);
+        this.superAttackPoint = 0;
+    }
+
+    // 攻撃（主人公の場合は、通常攻撃3回ごとに必殺技を使える）
+    attack() {
+        this.superAttackPoint++;
+        return super.attack();
+    }
+    // 必殺技を実行するメソッド
+    superAttack() {
+        if (this.superAttackPoint < 3) {
+            return -1;
+        }
+
+        // 必殺技を使ったら、必殺技ポイントはリセットされる
+        this.superAttackPoint = 0;
+
+        //　必殺技は、通常攻撃の2倍
+        const superAttackPoint = super.attack() * 2;
+        return superAttackPoint;
+    }
+}
+
+  //敵のクラスを作成する
+class Enemy extends Character {
+    // 獲得できるポイント（経験値）
+    getPoint() {}
+    
+}
+
+// 主人公を生成する
+const hero = new Hero("勇者", 30);
+
+function dispFields(message) {
+    // メッセージエリア表示
+    const messageArea = $('#messagearea');
+    messageArea.text(message);
+
     // 左矢印ボタン作成
     const leftButton = $('<button>', {
         text: '←'
@@ -76,20 +129,12 @@ function addButtonEvent(buttonArray, indexArray) {
     indexArray.forEach(element => {
         const btn = buttonArray[element];
         btn.on('click', () => {
-            dispFields();
+            dispFields("");
         });
     });
 }
 
 function encount() {
-    // 全ての矢印ボタンはHTMLから表示を消す
-    const leftArea = $('#left');
-    leftArea.text("");
-    const centerArea = $('#center');
-    centerArea.text("");
-    const rigthArea = $('#right');
-    rigthArea.text("");
-
     // 敵の画像を表示する
     const bgArea = $('#bgarea');
     bgArea.text("");
@@ -99,12 +144,72 @@ function encount() {
     });
     bgArea.append(img);
 
-    // ゲームを最初からスタートさせるボタンを表示する。
-    const restartBtn = $('<button>', {
-        text:'最初に戻る'
+    // 敵を生成する
+    const enemy = new Enemy('普通の敵', 15);
+
+    // メッセージを表示する
+    const messageArea = $('#messagearea');
+    messageArea.text('敵が現れた！');
+
+    // 戦闘用のボタンを表示する
+    // 通常攻撃のボタンを表示する
+    const leftArea = $('#left');
+    leftArea.text("");
+    const attackBtn = $('<button>', {
+        text:'通常攻撃'
     });
-    restartBtn.on('click', () => {
-        dispFields();
+    attackBtn.on('click', () => {
+        // 主人公が攻撃をする
+        const heroAp = hero.attack();
+        enemy.hp = enemy.hp - heroAp;
+
+        if (enemy.hp <= 0) {
+            // 敵が倒せたら通常のフィールドを表示する
+            dispFields("敵を倒した。");
+        } else {
+            // 敵に攻撃したら、主人公の与えたダメージを表示する。
+            messageArea.text(`勇者は${heroAp}のダメージを与えた。`);
+        }
+
     });
-    centerArea.append(restartBtn);
+    leftArea.append(attackBtn);
+
+    // 必殺技攻撃のボタンを表示する
+    const centerArea = $('#center');
+    centerArea.text("");
+    const superAttackBtn = $('<button>', {
+        text:'必殺技'
+    });
+    superAttackBtn.on('click', () => {
+        // 主人公が必殺技攻撃をする
+        const heroAp = hero.superAttack();
+
+        if (heroAp < 0) {
+            // 攻撃ポイントがマイナスの場合、必殺技は使えないメッセージを表示する
+            messageArea.text("必殺技はまだ使えない。");
+            return;
+        }
+        enemy.hp = enemy.hp - heroAp;
+
+        if (enemy.hp <= 0) {
+            // 敵が倒せたら通常のフィールドを表示する
+            dispFields("敵を倒しました。");
+        } else {
+            // 敵に攻撃したら、主人公の与えたダメージを表示する。
+            messageArea.text(`勇者は必殺技で${heroAp}のダメージを与えた。`);
+        }
+    });
+    centerArea.append(superAttackBtn);
+
+    // 逃げるボタンを表示する
+    const rightArea = $('#right');
+    rightArea.text("");
+    const escapeBtn = $('<button>', {
+        text:'逃げる'
+    });
+    escapeBtn.on('click', () => {
+        // 通常のフィールドを表示する
+        dispFields("敵から逃げられた。");
+    });
+    rightArea.append(escapeBtn);
 }
